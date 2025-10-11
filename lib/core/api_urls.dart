f8968d0d16adeabbfef54fc7bb306efa74f875ca
aliftheprogramer <alif.5220411242@student.uty.ac.id>
2025-10-12 00:06:27 +0700

    feat: Update API base URL to production endpoint

--- lib/core/api_urls.dart
+++ lib/core/api_urls.dart
@@ -1,3 +1,5 @@
 class ApiUrls {
-static const String baseUrl = "http://localhost:8080/api/";
+static const String baseUrl = "https://booking-badminton-api.vercel.app/api/";
+// static const String baseUrl = "http://localhost:8080/api/";
+
 }
